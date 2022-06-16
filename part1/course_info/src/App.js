56ffc71e3a817818d59453e4e42fd875e8d616dc
John Bautista <jnl.bautista.cs@gmail.com>
2022-06-16 12:59:12 +0100

    complete 1.2

--- part1/course_info/src/App.js
+++ part1/course_info/src/App.js
@@ -9,14 +9,18 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
+      <Part part={props.part1} exercise={props.exercise1} />
+      <Part part={props.part2} exercise={props.exercise2} />
+      <Part part={props.part3} exercise={props.exercise3} />
+    </div>
+  );
+};
+
+const Part = (props) => {
+  return (
+    <div>
       <p>
-        {props.part1} {props.exercise1}
-      </p>
-      <p>
-        {props.part2} {props.exercise2}
-      </p>
-      <p>
-        {props.part3} {props.exercise3}
+        {props.part} {props.exercise}
       </p>
     </div>
   );
