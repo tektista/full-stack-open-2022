eeb6354f299cb9ce9f5490a7ecc757e668b910ea
John Bautista <jnl.bautista.cs@gmail.com>
2022-07-20 18:01:14 +0100

    complete 1.9

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,11 +1,24 @@
 import { useState } from "react";
 
-const Statistic = ({ text, value }) => {
-  return (
-    <div>
-      {text} {value}
-    </div>
-  );
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
+  const average = (good - bad) / total;
+  const positive = good / total;
+
+  if (total > 0) {
+    return (
+      <>
+        <div>good {good}</div>
+        <div>neutral {neutral}</div>
+        <div>bad {bad}</div>
+        <div>all {total}</div>
+        <div>average {average} </div>
+        <div>positive {positive} %</div>
+      </>
+    );
+  }
+
+  return <div>No feedback given</div>;
 };
 
 const App = () => {
@@ -13,10 +26,6 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-
-  const total = good + bad + neutral;
-  const average = (good - bad) / total;
-  const positive = good / total;
 
   const handleGoodClick = () => {
     setGood(good + 1);
@@ -41,12 +50,7 @@ const App = () => {
 
       <div>
         <h2>Statistics</h2>
-        <Statistic text="good" value={good} />
-        <Statistic text="neutral" value={neutral} />
-        <Statistic text="bad" value={bad} />
-        <Statistic text="all" value={total} />
-        <Statistic text="average" value={average} />
-        <Statistic text="positive" value={positive} />
+        <Statistics good={good} neutral={neutral} bad={bad} />
       </div>
     </div>
   );
