91be1617e600498bfd8bf0b6f487ba452a4226e7
tektista <jnl.bautista.cs@gmail.com>
2022-10-13 09:06:12 +0100

    complete 2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,38 +1,40 @@
-const Header = ({ courseName }) => <h1>{courseName}</h1>;
+import Course from './components/Course'
 
-const Total = ({ partArray }) => {
-  const total = partArray.reduce(
-    (prevValue, currentValue) => prevValue + currentValue.exercises,
-    0
-  );
-  return <p>Total: {total}</p>;
-};
+// const Header = ({ courseName }) => <h1>{courseName}</h1>;
 
-const Part = ({ partName, noOfExercises }) => (
-  <>
-    {partName} {noOfExercises}
-  </>
-);
+// const Total = ({ partArray }) => {
+//   const total = partArray.reduce(
+//     (prevValue, currentValue) => prevValue + currentValue.exercises,
+//     0
+//   );
+//   return <p>Total: {total}</p>;
+// };
 
-const Content = ({ course }) => (
-  <>
-    {course.parts.map((part) => (
-      <li key={part.id}>
-        {" "}
-        <Part partName={part.name} noOfExercises={part.exercises} />
-      </li>
-    ))}
-  </>
-);
+// const Part = ({ partName, noOfExercises }) => (
+//   <>
+//     {partName} {noOfExercises}
+//   </>
+// );
 
-const Course = ({ header, content, total }) => (
-  <>
-    {/* use map here */}
-    <Header courseName={header} />
-    <Content course={content} />
-    <Total partArray={total} />
-  </>
-);
+// const Content = ({ course }) => (
+//   <>
+//     {course.parts.map((part) => (
+//       <li key={part.id}>
+//         {" "}
+//         <Part partName={part.name} noOfExercises={part.exercises} />
+//       </li>
+//     ))}
+//   </>
+// );
+
+// const Course = ({ header, content, total }) => (
+//   <>
+//     {/* use map here */}
+//     <Header courseName={header} />
+//     <Content course={content} />
+//     <Total partArray={total} />
+//   </>
+// );
 
 const Courses = ({ courseArray }) => (
   <>
---
+++ part2/courseinfo/src/components/Course.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const Header = ({ courseName }) => <h1>{courseName}</h1>;
+
+const Total = ({ partArray }) => {
+  const total = partArray.reduce(
+    (prevValue, currentValue) => prevValue + currentValue.exercises,
+    0
+  );
+  return <p>Total: {total}</p>;
+};
+
+const Part = ({ partName, noOfExercises }) => (
+  <>
+    {partName} {noOfExercises}
+  </>
+);
+
+const Content = ({ course }) => (
+  <>
+    {course.parts.map((part) => (
+      <li key={part.id}>
+        {" "}
+        <Part partName={part.name} noOfExercises={part.exercises} />
+      </li>
+    ))}
+  </>
+);
+
+const Course = ({ header, content, total }) => (
+  <>
+    {/* use map here */}
+    <Header courseName={header} />
+    <Content course={content} />
+    <Total partArray={total} />
+  </>
+);
+
+
+
+export default Course;
