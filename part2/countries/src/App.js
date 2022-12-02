563298bd08b2fd63cbf23a57582d617741c142f4
tektista <jnl.bautista.cs@gmail.com>
2022-12-02 15:31:43 +0000

    complete 2.14

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-// - Your API key is 044166a82531a2aade90c91a59fb074b
+// const Weather = ({ weather }) => {
+//   return <div>{!weather ? <p>...</p> : <>Weather: {weather.main.temp}</>}</div>;
+// };
 
 const Filter = ({ onChange }) => (
   <>
@@ -11,22 +13,26 @@ const Filter = ({ onChange }) => (
   </>
 );
 
-const Countries = ({ updatedCountries, showClickHandler }) => {
+const Countries = ({ countriesToDisplay, showClickHandler }) => {
   //if list of updated countries is one, show the view for that remaining country
-  if (updatedCountries.length === 1) {
-    const country = updatedCountries[0];
-    return <CountryData c={country} />;
+  if (countriesToDisplay.length === 1) {
+    const country = countriesToDisplay[0];
+    return (
+      <>
+        <CountryData c={country} />;
+      </>
+    );
   }
 
   //if list is greater than 10, too many
-  if (updatedCountries.length > 10) {
+  if (countriesToDisplay.length > 10) {
     return <div>Too many matches, specify another filter.</div>;
   }
 
   //otherwise return each country, with a show button
-  return updatedCountries.map((country) => (
+  return countriesToDisplay.map((country) => (
     <div key={country.name.official}>
-      {country.name.common}{" "}
+      {country.name.common}
       {/* when clicked, pass the common name of the clicked country to showClickHandler */}
       <button onClick={() => showClickHandler(country.name.common)}>
         show
@@ -37,10 +43,30 @@ const Countries = ({ updatedCountries, showClickHandler }) => {
 
 const CountryData = ({ c }) => {
   const languageList = [];
-
   for (const [key, value] of Object.entries(c.languages)) {
     languageList.push(value);
   }
+
+  const WEATHER_API_KEY = process.env.REACT_APP_API_KEY;
+
+  const [weather, setWeather] = useState(0);
+  const [wind, setWind] = useState(0);
+  const capital = c.capital.map((element) => element);
+  const capitals = c.capital[0];
+
+  useEffect(() => {
+    console.log("hello");
+
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${capitals}&appid=${WEATHER_API_KEY}&units=${"metric"}`
+      )
+      .then((response) => {
+        console.log(response.data);
+        setWeather(response.data.main.temp);
+        setWind(response.data.wind.speed);
+      });
+  }, [capitals]);
 
   return (
     <>
@@ -51,27 +77,45 @@ const CountryData = ({ c }) => {
           <li key={name}> {name}</li>
         ))}
       </div>
-
       <h2>{"languages: "}</h2>
       <div>
-        {" "}
         {languageList.map((language) => (
-          <li>{language}</li>
+          <ul key={language}>{language}</ul>
         ))}
       </div>
-
       <div>
-        <img src={c.flag} alt="Country Flag" width="8%"></img>
+        <img src={c.flags.png} alt="Country Flag" width="8%"></img>
       </div>
+
+      <h2>
+        Weather in{" "}
+        {c.capital.map((name) => (
+          <li key={name}> {name}</li>
+        ))}
+      </h2>
+
+      <div>Weather: {weather} celsius</div>
+      <div>Wind: {wind} m/s</div>
     </>
   );
 };
-const App = () => {
-  const [countries, setCountries] = useState([]);
-  const [countryName, setCountryName] = useState("");
-  const [updatedCountries, setUpdatedCountries] = useState([]);
 
-  // const [showResult, setShowResult] = useState(false);
+const App = () => {
+  //country list
+  const [countries, setCountries] = useState([]);
+
+  //search
+  const [countryName, setCountryName] = useState("");
+
+  //weather
+  // const [weather, setWeather] = useState([]);
+
+  // const [updatedCountries, setUpdatedCountries] = useState([]);
+
+  //array of countries to display based on filter
+  const countriesToDisplay = countries.filter((country) =>
+    country.name.common.toLowerCase().includes(countryName.toLowerCase())
+  );
 
   //pull data
   useEffect(() => {
@@ -79,8 +123,32 @@ const App = () => {
     axios.get("https://restcountries.com/v3.1/all").then((response) => {
       console.log("promise fulfilled");
       setCountries(response.data);
+      console.log(response.data);
     });
   }, []);
+
+  // useEffect(() => {
+  //   const WEATHER_API_KEY = process.env.REACT_APP_API_KEY;
+
+  //   const countriesToDisplay = countries.filter((country) =>
+  //     country.name.common.toLowerCase().includes(countryName.toLowerCase())
+  //   );
+
+  //   if (countriesToDisplay.length === 1) {
+  //     console.log("HELLo");
+
+  //     const country = countriesToDisplay[0];
+  //     axios
+  //       .get(
+  //         `https://api.openweathermap.org/data/2.5/weather?q=${
+  //           country.capital[0]
+  //         }&appid=${WEATHER_API_KEY}&units=${"metric"}`
+  //       )
+  //       .then((response) => {
+  //         setWeather(response.data);
+  //       });
+  //   }
+  // }, [countries, countryName]);
 
   console.log("render", countries.length, "countries");
 
@@ -88,37 +156,36 @@ const App = () => {
   const handleNameChange = (event) => {
     //set CountryName to the value of the input field every time it is pressed
     setCountryName(event.target.value);
+    console.log(countriesToDisplay);
 
     //filter countries based on updated country name in input field
-    setUpdatedCountries(
-      countries.filter((country) =>
-        country.name.common.toLowerCase().includes(countryName.toLowerCase())
-      )
-    );
+    // setUpdatedCountries(
+    //   countries.filter((country) =>
+    //     country.name.common.toLowerCase().includes(countryName.toLowerCase())
+    //   )
+    // );
   };
 
   //handler for showing the view of each country
   const handleShowClick = (countryName) => {
     //update the filter to
     setCountryName(countryName);
-
-    //filter countries based on updated country name in input field
-    setUpdatedCountries(
-      countries.filter((country) =>
-        country.name.common.toLowerCase().includes(countryName.toLowerCase())
-      )
-    );
   };
+
+  //filter countries based on updated country name in input field
+  // setUpdatedCountries(
+  //   countries.filter((country) =>
+  //     country.name.common.toLowerCase().includes(countryName.toLowerCase())
+  //   )
+  // );
 
   return (
     <>
       <Filter onChange={handleNameChange} />
       <Countries
-        updatedCountries={updatedCountries}
+        countriesToDisplay={countriesToDisplay}
         showClickHandler={handleShowClick}
       />
-
-      
     </>
   );
 };
