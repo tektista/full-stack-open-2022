0cd32dc5a1bf0b889dac75943089e88eccae6026
tektista <jnl.bautista.cs@gmail.com>
2022-12-04 15:27:32 +0000

    complete 3.7

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,8 +1,13 @@
 const { response } = require("express");
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
 app.use(express.json());
+
+app.use(morgan("tiny"));
+
+
 
 let persons = [
   {
