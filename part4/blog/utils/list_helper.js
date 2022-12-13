87b684ab3526f8567aaaae9fa4c3a16c9de3e12b
tektista <jnl.bautista.cs@gmail.com>
2022-12-13 13:05:22 +0000

    complete 4.4

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -1,7 +1,18 @@
 const dummy = (blogs) => {
-    return 1
-  }
-  
-  module.exports = {
-    dummy
-  }
+  return 1;
+};
+
+const totalLikes = (blogList) => {
+  let totalLikes = 0;
+
+  blogList.forEach((blog) => {
+    totalLikes = totalLikes + blog.likes;
+  });
+
+  return totalLikes;
+};
+
+module.exports = {
+  dummy,
+  totalLikes,
+};
