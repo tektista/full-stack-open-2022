2c1a0b6acae40811b744bdeadab89f4dda157b9c
tektista <jnl.bautista.cs@gmail.com>
2022-12-13 15:18:33 +0000

    complete 4.6

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -24,8 +24,56 @@ const favouriteBlog = (blogList) => {
   };
 };
 
+const mostBlogs = (blogList) => {
+  //for each blog in the bloglist, check if that author exists in authorBlogCount
+  //If the author already exists increment their blog count
+  //If they dont push the object into the author blog count
+
+  const authorBlogCount = [];
+
+  blogList.forEach((blog) => {
+    const currentAuthor = blog.author;
+    const authorExists = authorBlogCount.find(
+      (authorBlog) => authorBlog.author === currentAuthor
+    );
+
+    console.log();
+
+    if (authorExists) {
+      authorBlogCount.forEach((authorObj) => {
+        if (authorObj.author === currentAuthor) {
+          authorObj.blogs = authorObj.blogs + 1;
+        }
+      });
+    } else {
+      authorBlogCount.push({
+        author: currentAuthor,
+        blogs: 1,
+      });
+    }
+  });
+
+  const authorWithMostBlogs = authorBlogCount.reduce((prev, curr) => {
+    return prev.blogs > curr.blogs ? prev : curr;
+  });
+
+  return {
+    author: authorWithMostBlogs.author,
+    blogs: authorWithMostBlogs.blogs,
+  };
+
+  // blogList.forEach((blog) => {
+  //     const author = blog.author
+  //     const authorExists = authorBlogCount.find((authorBlog) => authorBlog.author === author)
+  //     if (authorExists) {
+  //         authorExists.blogs = authorExists.blogs + 1
+  //     } else {
+  //         authorBlogCount.push({
+};
+
 module.exports = {
   dummy,
   totalLikes,
   favouriteBlog,
+  mostBlogs,
 };
